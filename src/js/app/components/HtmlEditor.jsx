a4157b1ad77153856b8c773e5ee6cd8643291dab
StanleySong <sxhuan@gmail.com>
2017-06-14 13:35:34 -0700

    add copy to clipboard

--- src/js/app/components/HtmlEditor.jsx
+++ src/js/app/components/HtmlEditor.jsx
@@ -5,6 +5,8 @@
 import React from 'react';
 import ReactQuill from 'react-quill';
 import ContentWrapper from './ContentWrapper';
+import {Row, Col, Button} from 'antd';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 export default class HtmlEditor extends React.Component {
     constructor(props) {
@@ -14,14 +16,28 @@ export default class HtmlEditor extends React.Component {
 
     handleChange(value) {
         this.setState({ text: value })
+        console.log(this.state.text);
     }
 
     render() {
         return (
             <ContentWrapper>
-            <ReactQuill theme="snow" value={this.state.text} onChange={this.handleChange}>
-                <div className="my-editing-area"/>
-            </ReactQuill>
+                <Row>
+                    <Col span={12}>
+                        <ReactQuill theme="snow" value={this.state.text} onChange={this.handleChange.bind(this)}>
+                            <div className="my-editing-area"/>
+                        </ReactQuill>
+                    </Col>
+                    <Col span={12}>
+                        <div className="tool-bar">
+                            <CopyToClipboard text={this.state.text}
+                                onCopy={() => this.setState({copied: true})}>
+                                <Button type="primary">Copy To Clipboard</Button>
+                            </CopyToClipboard>
+                        </div>
+                        {this.state.text}
+                    </Col>
+                </Row>
             </ContentWrapper>
         )
     }
